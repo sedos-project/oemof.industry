e7c41579b50281546a3d2494260d514be320bcac
Hendrik Huyskens <hendrik.huyskens@rl-institut.de>
2024-02-23 13:13:28 +0100

    Implemented IIS_CHPSTMGAS101_LB datapackage successfully

--- tests/test_datapackage.py
+++ tests/test_datapackage.py
@@ -1,26 +1,22 @@
 import pathlib
-import pytest
 
 from oemof.solph import EnergySystem, Model, processing
 from oemof.tabular import datapackage  # noqa
-
-# from oemof.tabular.postprocessing import calculations
 from oemof.tabular.facades import Bus, Commodity, Conversion, Excess, Load
 
 from oemof_industry.mimo_converter import MIMO, MultiInputMultiOutputConverterBlock
+from tests.test_mimo_converter import check_results_for_IIS_CHPSTMGAS101_LB
 
 
 class TestMimoFacade:
-    # @classmethod
-    # def setup_class(cls):
-
-
     @classmethod
     def setup_method(cls):
         MIMO_DATAPACKAGE = (
-                pathlib.Path(
-                    # __file__).parent / "datapackages" / "mimo" / "exo_steel" / "datapackage.json"
-                    __file__).parent / "datapackages" / "mimo" / "IIS_CHPSTMGAS101_LB" / "datapackage.json"
+            pathlib.Path(__file__).parent
+            / "datapackages"
+            / "mimo"
+            / "IIS_CHPSTMGAS101_LB"
+            / "datapackage.json"
         )
         TYPEMAP = {
             "bus": Bus,
@@ -34,69 +30,51 @@ class TestMimoFacade:
             str(MIMO_DATAPACKAGE), attributemap={}, typemap=TYPEMAP
         )
 
-    def test_model_structure(self):# todo note: test with "exo_steel"
-
-        for mimo in self.es.groups[MultiInputMultiOutputConverterBlock]:  # todo - how to use `set`
-
+    def test_model_structure(self):  # todo note: test with "exo_steel"
+        for mimo in self.es.groups[
+            MultiInputMultiOutputConverterBlock
+        ]:  # todo - how to use `set`
             # "gas" is input group
             group_names = [input for input in mimo.input_groups.keys()]
             assert "gas" in group_names
 
             # check the group's buses (labels)
-            group_bus_labels = [bus.label for bus in
-                                mimo.input_groups["gas"].keys()]
+            group_bus_labels = [bus.label for bus in mimo.input_groups["gas"].keys()]
             assert group_bus_labels == ["sec_H2", "ch4"]
 
             # check input buses
             input_buses = list(mimo.input_groups.values())[1:]
             input_labels = [list(bus.keys())[0].label for bus in input_buses]
-            assert input_labels == ["electricity", "sec_coke_oven_gas", "iip_steel_crudesteel", "sec_H2", "ch4"]
+            assert input_labels == [
+                "electricity",
+                "sec_coke_oven_gas",
+                "iip_steel_crudesteel",
+                "sec_H2",
+                "ch4",
+            ]
 
             # check output buses (no groups)
             output_buses = list(mimo.output_groups.values())
             output_labels = [list(bus.keys())[0].label for bus in output_buses]
-            assert output_labels == ["exo_steel", "co2_em", "ch4_em", "n2o_em"]  # todo fails
+            assert output_labels == [
+                "exo_steel",
+                "co2_em",
+                "ch4_em",
+                "n2o_em",
+            ]  # todo fails
 
             # check other parameters
             assert mimo.conversion_factors["gas"].default == 5
             # assert mimo.conversion_factors["sec_coke_oven_gas"].default == 2.2  # todo
             # assert mimo.conversion_factors["electricity"].default == 0.3
 
-    def test_results_IIS_CHPSTMGAS101_LB(self):  # todo note: test with IIS_CHPSTMGAS101_LB
+    def test_results_IIS_CHPSTMGAS101_LB(self):
         m = Model(self.es)
         m.solve("cbc")
 
         # create result object
         results = processing.convert_keys_to_strings(processing.results(m))
-
-        assert results[("gas", "mimo")]["sequences"]["flow"].values[
-                   0
-               ] == pytest.approx(5.4945, abs=1e-3)
-        assert results[("gas", "mimo")]["sequences"]["flow"].values[
-                   1
-               ] == pytest.approx(0)
-        assert results[("hydro", "mimo")]["sequences"]["flow"].values[
-                   0
-               ] == pytest.approx(5.4945, abs=1e-2)
-        assert results[("hydro", "mimo")]["sequences"]["flow"].values[
-                   1
-               ] == pytest.approx(10.8696, abs=1e-3)
-        assert results[("mimo", "ch4_em")]["sequences"]["flow"].values[
-                   0] == pytest.approx(
-            0.0149, abs=1e-3
-        )
-        assert results[("mimo", "ch4_em")]["sequences"]["flow"].values[
-                   1] == pytest.approx(
-            0.0148, abs=1e-3
-        )
-        assert results[("mimo", "co2_em")]["sequences"]["flow"].values[
-                   0] == pytest.approx(
-            307.6923, abs=1e-2
-        )
-        assert results[("mimo", "co2_em")]["sequences"]["flow"].values[
-                   1] == pytest.approx(
-            0
-        )
+        check_results_for_IIS_CHPSTMGAS101_LB(results)
 
     def test_solving_and_postprocessing(self):
         m = Model(self.es)

46ab45466737e7d3b76dfabaf0c8e99749f11caa
Hendrik Huyskens <hendrik.huyskens@rl-institut.de>
2024-02-23 13:48:33 +0100

    Add datapackage test to test investment

--- tests/test_datapackage.py
+++ tests/test_datapackage.py
@@ -6,31 +6,52 @@ from oemof.tabular.facades import Bus, Commodity, Conversion, Excess, Load
 
 from oemof_industry.mimo_converter import MIMO
 from tests.test_mimo_converter import check_results_for_IIS_CHPSTMGAS101_LB
+from tests.test_mimo_investment import check_results_for_investment_var_output
+
+
+TYPEMAP = {
+    "bus": Bus,
+    "commodity": Commodity,
+    "conversion": Conversion,
+    "excess": Excess,
+    "load": Load,
+    "mimo": MIMO,
+}
 
 
 def test_datapackage_results_for_IIS_CHPSTMGAS101_LB():
-    MIMO_DATAPACKAGE = (
+    datapackage_path = (
         pathlib.Path(__file__).parent
         / "datapackages"
         / "mimo"
         / "IIS_CHPSTMGAS101_LB"
         / "datapackage.json"
     )
-    TYPEMAP = {
-        "bus": Bus,
-        "commodity": Commodity,
-        "conversion": Conversion,
-        "excess": Excess,
-        "load": Load,
-        "mimo": MIMO,
-    }
     es = EnergySystem.from_datapackage(
-        str(MIMO_DATAPACKAGE), attributemap={}, typemap=TYPEMAP
+        str(datapackage_path), attributemap={}, typemap=TYPEMAP
     )
 
     m = Model(es)
     m.solve("cbc")
 
-    # create result object
     results = processing.convert_keys_to_strings(processing.results(m))
     check_results_for_IIS_CHPSTMGAS101_LB(results)
+
+
+def test_datapackage_results_for_investment_var_outputs():
+    datapackage_path = (
+        pathlib.Path(__file__).parent
+        / "datapackages"
+        / "mimo"
+        / "investment_var_output"
+        / "datapackage.json"
+    )
+    es = EnergySystem.from_datapackage(
+        str(datapackage_path), attributemap={}, typemap=TYPEMAP
+    )
+
+    m = Model(es)
+    m.solve("cbc")
+
+    results = processing.convert_keys_to_strings(processing.results(m))
+    check_results_for_investment_var_output(results)
